e459b818c38741674b83fa455034f58c1fb21396
m21power <150428485+m21power@users.noreply.github.com>
2024-02-15 10:38:25 +0300

    Add solution for Design Browser History

--- progress/leetcode/design-browser-history.py
+++ progress/leetcode/design-browser-history.py
@@ -1,4 +1,5 @@
-class BrowserHistory:
+"""USING STACK"""
+"""class BrowserHistory:
 
     def __init__(self, homepage: str):
         self.history=[]
@@ -22,8 +23,37 @@ class BrowserHistory:
             self.history.append(self.front.pop(-1))
             steps-=1
         return self.history[-1]
-        
+        """
+class Node:
+  def __init__(self, url: str):
+    self.prev = None
+    self.next = None
+    self.url = url
 
+
+class BrowserHistory:
+  def __init__(self, homepage: str):
+    self.curr = Node(homepage)
+
+  def visit(self, url: str) -> None:
+    new_node=Node(url)
+    self.curr.next = new_node
+    self.curr.next.prev = self.curr
+    self.curr = new_node
+    """the head continued changing within the current position if u try to access
+    the current position of the vist it shows you the last value of the visit thats how the above
+    algorithm works"""
+  def back(self, steps: int) -> str:
+    while self.curr.prev and steps > 0:
+      self.curr = self.curr.prev
+      steps -= 1
+    return self.curr.url
+
+  def forward(self, steps: int) -> str:
+    while self.curr.next and steps > 0:
+      self.curr = self.curr.next
+      steps -= 1
+    return self.curr.url
 
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
