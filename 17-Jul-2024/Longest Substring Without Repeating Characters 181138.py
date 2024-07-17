16403924fb679979ef085f52068c2695c840e453
Mesay Lemma <150428485+m21power@users.noreply.github.com>
2024-07-17 10:57:55 +0300

    Created 'Longest Substring Without Repeating Characters' by A2SV Hub

---
+++ 17-Jul-2024/Longest Substring Without Repeating Characters 181138.py
@@ -0,0 +1,18 @@
+# Problem: Longest Substring Without Repeating Characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        dic = defaultdict(int)
+        left = res = 0
+        for i,let in enumerate(s):
+
+            if let in dic and dic[let] >= left:
+                left = dic[let]+1
+                dic[let] = i
+            else:
+                dic[let] = i
+
+            res = max(res,i-left+1)
+            
+        return res
