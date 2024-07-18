d289876f2c785c7ff5edb4867fb9bd048e4058cc
Mesay Lemma <150428485+m21power@users.noreply.github.com>
2024-07-18 08:11:20 +0300

    Created 'Z-Sort' by A2SV Hub

---
+++ 18-Jul-2024/Z-Sort 181445.py
@@ -0,0 +1,18 @@
+# Problem: Z-Sort - https://codeforces.com/problemset/problem/652/B
+
+def fun(arr):
+    i = 1
+    while i < len(arr):
+        if (i+1) % 2 == 0:
+            if arr[i] < arr[i-1]:
+                arr[i],arr[i-1] = arr[i-1],arr[i]
+        else:
+            if arr[i] > arr[i-1]:
+                arr[i],arr[i-1] = arr[i-1],arr[i]
+        i+=1
+    return arr
+
+n = int(input())
+arr = list(map(int,input().split()))
+for n in fun(arr):
+    print(n,end = " ")
