b2a0054239d2a76fdb48249264431564c7274322
Mesay Lemma <150428485+m21power@users.noreply.github.com>
2024-08-12 20:14:55 +0300

    Created 'A - A2SV Contest #25' by A2SV Hub

---
+++ A2SV G5 - Contest #25 12-Aug-2024/A - A2SV Contest #25 190916.py
@@ -0,0 +1,22 @@
+# Problem: A - A2SV Contest #25 - https://codeforces.com/gym/535749/problem/A
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+a, b, c, d = map(int, input().split())
+
+def find_points(p, t):
+    return max((3*p)//10, p - (p*t)//250)
+
+
+Misha = find_points(a, c)
+Vasya = find_points(b, d)
+
+if Misha > Vasya:
+    print('Misha')
+
+elif Misha < Vasya:
+    print('Vasya')
+
+else:
+    print('Tie')
