6837edfe06c95513136d3c4658ee9f3fc355faa0
Mesay Lemma <150428485+m21power@users.noreply.github.com>
2024-08-12 20:14:45 +0300

    Created 'B - Optimal Point' by A2SV Hub

---
+++ A2SV G5 - Contest #25 12-Aug-2024/B - Optimal Point 190915.py
@@ -0,0 +1,18 @@
+# Problem: B - Optimal Point - https://codeforces.com/gym/535749/problem/B
+
+for _ in range(int(input())):
+    N, K = map(int, input().split())
+    
+    l_ok = r_ok = False
+    for _ in range(N):
+        l, r = map(int, input().split())
+        
+        if l == K:
+            l_ok = True
+        if r == K:
+            r_ok = True
+    
+    if l_ok and r_ok:
+        print("YES")
+    else:
+        print("NO")
