b05664da57612ba6efd004a071b73f4983eb0eb8
Mesay Lemma <150428485+m21power@users.noreply.github.com>
2024-09-04 20:26:45 +0300

    Created 'B - Yikes!' by A2SV Hub

---
+++ A2SV G5 - Contest #8 04-Sep-2024/B - Yikes! 211052.py
@@ -0,0 +1,26 @@
+# Problem: B - Yikes! - https://codeforces.com/gym/511242/problem/B
+
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+q = list(map(int, input().split()))
+
+
+prefix_sum = [0]
+for pile in a:
+    prefix_sum.append(prefix_sum[-1] + pile)
+
+
+for label in q:
+    low = 0
+    high = n
+
+    while low <= high:
+        mid = low + (high - low)//2
+        if prefix_sum[mid] < label:
+            low = mid + 1
+        
+        else:
+            high = mid - 1
+    
+    print(low)
