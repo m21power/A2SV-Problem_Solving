f8c1162f5074118b251dbcd796a9c96ee71b675b
Mesay Lemma <150428485+m21power@users.noreply.github.com>
2024-09-07 21:06:31 +0300

    Created 'C - Painting Class' by A2SV Hub

---
+++ A2SV G5 - Contest #12 07-Sep-2024/C - Painting Class 213272.py
@@ -0,0 +1,26 @@
+# Problem: C - Painting Class - https://codeforces.com/gym/517685/problem/C
+
+from collections import deque
+from sys import stdin
+
+
+def input(): return stdin.readline().strip()
+
+N = int(input())
+
+parents = list(map(int, input().split()))
+color = list(map(int, input().split()))
+
+adj = [[] for _ in range(N)]
+for ch, p in enumerate(parents, 1):
+    adj[p - 1].append(ch)
+
+q = deque([(0, color[0])])
+ans = 1
+
+while q:
+    v, c = q.popleft()
+    for ch in adj[v]:
+        q.append((ch, color[ch]))
+        ans += (color[ch] != c)
+print(ans)
