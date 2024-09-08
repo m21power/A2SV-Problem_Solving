32867b27a44a736caf46890c009e44f47dde7f81
Mesay Lemma <150428485+m21power@users.noreply.github.com>
2024-09-08 20:54:01 +0300

    Created 'A - Balanced Alphabet' by A2SV Hub

---
+++ A2SV G5 - Contest #13 08-Sep-2024/A - Balanced Alphabet 213565.py
@@ -0,0 +1,16 @@
+# Problem: A - Balanced Alphabet - https://codeforces.com/gym/519135/problem/A
+
+t = int(input())
+for _ in range(t):
+    n,k = map(int,input().split())
+    
+    numbers = n // k
+    
+    remaining = n % k
+    
+    ans = ""
+    
+    for i in range(k):
+        ans += chr(i + ord("a")) * numbers
+    ans += "a" * remaining
+    print(ans)
