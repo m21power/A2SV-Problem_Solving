c5c7147705a2312a01e9e030cbe37ba9494b48d2
Mesay Lemma <150428485+m21power@users.noreply.github.com>
2024-09-10 20:02:45 +0300

    Created 'A - a-e-i-o-u-y' by A2SV Hub

---
+++ A2SV G5 - Contest #15 10-Sep-2024/A - a-e-i-o-u-y 214135.py
@@ -0,0 +1,15 @@
+# Problem: A - a-e-i-o-u-y - https://codeforces.com/gym/522079/problem/A
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+word = input()
+
+answer = []
+vowels = {'a', 'e', 'i', 'o', 'u', 'y'}
+for char in word:
+    if answer and answer[-1] in vowels and char in vowels:
+        continue
+    answer.append(char)
+print(*answer, sep="")
