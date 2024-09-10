5e6b368b66a9995bc481311e6840e27a77fff704
Mesay Lemma <150428485+m21power@users.noreply.github.com>
2024-09-10 20:02:54 +0300

    Created 'B - tourist orz!' by A2SV Hub

---
+++ A2SV G5 - Contest #15 10-Sep-2024/B - tourist orz! 214136.py
@@ -0,0 +1,12 @@
+# Problem: B - tourist orz! - https://codeforces.com/gym/522079/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, z = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    max_val = 0
+    for num in a:
+        max_val = max(max_val, num | z)
+
+    print(max_val)
