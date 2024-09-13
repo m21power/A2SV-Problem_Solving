8a13cf3c319d0d3dc2641449ff3a760faf8666a6
Mesay Lemma <150428485+m21power@users.noreply.github.com>
2024-09-13 20:41:07 +0300

    Created 'A - Distribute' by A2SV Hub

---
+++ A2SV G5 - Contest #17 13-Sep-2024/A - Distribute 214909.py
@@ -0,0 +1,19 @@
+# Problem: A - Distribute - https://codeforces.com/gym/524965/problem/A
+
+n = int(input())
+ 
+nums = list(map(int, input().split()))
+ 
+ 
+for i in range(n): 
+    nums[i] = [nums[i], i+1]
+#print(nums)
+nums.sort()
+l = 0 
+r = n - 1
+ 
+while l < r: 
+ 
+    print(nums[l][1], nums[r][1]) 
+    l += 1
+    r -= 1
