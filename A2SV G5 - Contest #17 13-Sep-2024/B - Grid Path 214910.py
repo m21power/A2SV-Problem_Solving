47ecc2718b61e23e7c8f34155c941c62ca2e876a
Mesay Lemma <150428485+m21power@users.noreply.github.com>
2024-09-13 20:41:10 +0300

    Created 'B - Grid Path' by A2SV Hub

---
+++ A2SV G5 - Contest #17 13-Sep-2024/B - Grid Path 214910.py
@@ -0,0 +1,24 @@
+# Problem: B - Grid Path - https://codeforces.com/gym/524965/problem/B
+
+t = int(input()) 
+
+
+for _ in range(t): 
+
+
+    r, c , k = map(int, input().split())
+    
+
+    curr = 0
+
+    for i in range(c-1): 
+        curr += 1
+
+    for i in range(r-1): 
+        curr += c
+    
+
+    if curr == k: 
+        print("YES")
+    else:
+        print("NO")
