7424772eb21bba840c28fe8e472e93ae94098f80
Mesay Lemma <150428485+m21power@users.noreply.github.com>
2024-09-15 21:38:48 +0300

    Created 'B - Distinct Digits' by A2SV Hub

---
+++ A2SV G5 - Contest #21 15-Sep-2024/B - Distinct Digits 215322.py
@@ -0,0 +1,12 @@
+# Problem: B - Distinct Digits - https://codeforces.com/gym/530187/problem/B
+
+for _ in range(int(input())):
+    n = int(input())
+
+    ans = []
+    for digit in range(9, 0, -1):
+        if n >= digit:
+            ans.append(digit)
+            n -= digit
+    ans.reverse()
+    print(*ans, sep="")
