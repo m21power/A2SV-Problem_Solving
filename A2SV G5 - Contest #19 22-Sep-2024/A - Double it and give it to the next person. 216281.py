41c5482446cafd39f1f65ec5ad5142dbc2821a5c
Mesay Lemma <150428485+m21power@users.noreply.github.com>
2024-09-22 08:49:23 +0300

    Created 'A - Double it and give it to the next person.' by A2SV Hub

---
+++ A2SV G5 - Contest #19 22-Sep-2024/A - Double it and give it to the next person. 216281.py
@@ -0,0 +1,9 @@
+# Problem: A - Double it and give it to the next person. - https://codeforces.com/gym/527294/problem/A
+
+def fun(st):
+    t = st[::-1]
+    return st + t
+
+for _ in range(int(input())):
+    st = input()
+    print(fun(st))
